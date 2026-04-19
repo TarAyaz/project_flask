3717ff2221a5c4b53560851b52d62b14eb2a35bf
Ayaz <ayaz.tarisov@yandex.ru>
2026-04-19 19:35:34 +0500

    create models for users, books and search history

---
+++ app/data/models.py
@@ -0,0 +1,50 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
+    email = sqlalchemy.Column(
+        sqlalchemy.String, index=True, unique=True, nullable=False
+    )
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    books = orm.relationship("Book", back_populates="user")
+    searches = orm.relationship("SearchQuery", back_populates="user")
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+class Book(SqlAlchemyBase):
+    __tablename__ = "books"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    author = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    isbn = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    language = sqlalchemy.Column(sqlalchemy.String, default="ru")
+    cover_url = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    timestamp = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship("User", back_populates="books")
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+
+class SearchQuery(SqlAlchemyBase):
+    __tablename__ = "search_history"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    query = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    timestamp = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship("User", back_populates="searches")
+
+    def __repr__(self):
+        return f"<Search {self.query}>"
