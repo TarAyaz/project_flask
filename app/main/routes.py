24ea5f52e63ac0a88a04ea32b0b6ae3fded3d62d
Ayaz <ayaz.tarisov@yandex.ru>
2026-04-20 15:01:50 +0500

    refactor: separate home and search pages, add hero banner

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,10 +6,15 @@ from app.utils.open_library_api import search_books
 bp = Blueprint("main", __name__)
 
 
-@bp.route("/", methods=["GET", "POST"])
+@bp.route("/")
 def index():
+    return render_template("index.html", title="Добро пожаловать")
+
+
+@bp.route("/search", methods=["GET", "POST"])
+def search():
     form = SearchForm()
     books = []
     if form.validate_on_submit():
         books = search_books(form.query.data)
-    return render_template("index.html", title="Главная", form=form, books=books)
+    return render_template("search.html", title="Главная", form=form, books=books)
